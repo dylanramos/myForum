3981590b993f1c9dea7a5547bd4ed2c4c04f20e0
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2020-12-01 08:50:42 +0100

    Have all the empty controllers

--- app/Http/Controllers/ReferenceController.php
+++ app/Http/Controllers/ReferenceController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class OpinionController extends Controller
+class ReferenceController extends Controller
 {
     /**
      * Display a listing of the resource.
--- app/Http/Controllers/StateController.php
+++ app/Http/Controllers/StateController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class OpinionController extends Controller
+class StateController extends Controller
 {
     /**
      * Display a listing of the resource.
