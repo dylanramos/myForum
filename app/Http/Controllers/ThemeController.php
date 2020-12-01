c1689572e061b5984e4d38cc16189d83d77167e6
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2020-12-01 11:57:18 +0100

    Have a functional ThemeController index

--- app/Http/Controllers/ThemeController.php
+++ app/Http/Controllers/ThemeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Theme;
 
 class ThemeController extends Controller
 {
@@ -13,6 +14,8 @@ class ThemeController extends Controller
      */
     public function index()
     {
+        $themes = Theme::all();
+
         return view ('themes.index');
     }
 
