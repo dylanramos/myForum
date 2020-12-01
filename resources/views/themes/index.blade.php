0bcded7400d10d34bb00629b81799623b8fd62d6
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2020-12-01 12:23:39 +0100

    Show the list of the themes

--- resources/views/themes/index.blade.php
+++ resources/views/themes/index.blade.php
@@ -2,4 +2,11 @@
 
 @section ('content')
     <h1 class="text-center p-5">List Theme</h1>
+    @forelse($themes as $theme)
+        <div class="row">
+            <a href="">{{ $theme->name }}</a>
+        </div>
+    @empty
+        <h1>Aucun Thème</h1>
+    @endforelse
 @endsection
