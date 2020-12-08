21463eba7d7eb433482018da9e3f10f752cbbdcc
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2020-12-08 09:55:33 +0100

    Have the references edition page display

--- resources/views/references/update.blade.php
+++ resources/views/references/update.blade.php
@@ -1,5 +1,13 @@
 @extends ('layout')
 
 @section ('content')
-    <h1 class="text-center p-5">Update Reference <?= $id ?></h1>
+    <form action="{{ route('references.update',$reference->id) }}" method="post">
+        @method('PUT')
+        @csrf
+        @include('references.form_body')
+        <div class="row m-3">
+            <button class="btn btn-success">Enregistrer</button>
+            <a href="{{ url()->previous() }}" class="btn btn-primary">Annuler</a>
+        </div>
+    </form>
 @endsection
