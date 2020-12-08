788ae774df619419f4fce5e455809fd4e3b8d760
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2020-12-08 09:17:08 +0100

    Copy the references list script to the public directory

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .copy('node_modules/mdbootstrap', 'public/assets/mdbootstrap', false)
     .copy('node_modules/@fortawesome', 'public/assets/@fortawesome', false)
+    .js('resources/views/references/referenceslist.js', 'public/js/referenceslist.js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
