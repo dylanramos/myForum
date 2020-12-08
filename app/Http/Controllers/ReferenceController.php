b496969de319315fde70397e41731807acdb96b5
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2020-12-08 09:47:28 +0100

    Have the references show function

--- app/Http/Controllers/ReferenceController.php
+++ app/Http/Controllers/ReferenceController.php
@@ -54,7 +54,8 @@ class ReferenceController extends Controller
      */
     public function show($id)
     {
-        //
+        $reference = Reference::find($id);
+        return view ('references.show')->with(compact('reference'));
     }
 
     /**
