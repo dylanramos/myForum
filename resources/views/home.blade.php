d16f36959753d91cf85fc316fc61307a463ad3c1
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2020-12-17 09:46:00 +0100

    Have the home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,7 @@
 @extends ('layout')
 
 @section ('content')
-    <h1>Home</h1>
+    @foreach ($themes as $theme)
+        <a href="{{ route('themes.show',$theme->id) }}"><div class="border border-dark p-2">{{ $theme->name }} <span class="badge badge-light float-right">{{ $theme->topics->count() }}</span></div></a>
+    @endforeach
 @endsection
