e9bf736f5f9fd7ab29050c7cc6e2dec1fba3d739
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2020-12-17 11:59:54 +0100

    Have the opinions method for the reference class

--- app/Models/Reference.php
+++ app/Models/Reference.php
@@ -10,4 +10,9 @@ class Reference extends Model
     use HasFactory;
 
     public $timestamps = false; // To disable the created_at and updated_at default columns
+
+    public function opinions()
+    {
+        return $this->belongsToMany(Opinion::class);
+    }
 }
