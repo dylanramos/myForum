fd3fb0032e49f7733c6a429131f7c66c9d3efc8f
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2020-12-17 12:01:41 +0100

    Have the opninions method and the forumuser method for the topic class

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -20,4 +20,9 @@ class Topic extends Model
     {
         return $this->hasMany(Opinion::class);
     }
+
+    public function forumuser()
+    {
+        return $this->belongsTo(ForumUser::class);
+    }
 }
