a7e20dd37635841cba037f56c2f37748cdd24512
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2020-12-17 10:32:23 +0100

    Have the theme function for the topic class

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -10,4 +10,9 @@ class Topic extends Model
     use HasFactory;
 
     public $timestamps = false; // To disable the created_at and updated_at default columns
+
+    public function theme()
+    {
+        return $this->belongsTo(Theme::class);
+    }
 }
