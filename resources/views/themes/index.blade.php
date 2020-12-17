d4673cbf8f1991733d391730915d77a1116e0377
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2020-12-17 10:44:09 +0100

    Have the themes href in the index page

--- resources/views/themes/index.blade.php
+++ resources/views/themes/index.blade.php
@@ -4,7 +4,7 @@
     <h1 class="text-center p-5">List Theme</h1>
     @forelse($themes as $theme)
         <div class="row">
-            <a href="">{{ $theme->name }}</a>
+            <a href="{{ route('themes.show', $theme->id) }}">{{ $theme->name }}</a>
         </div>
     @empty
         <h1>Aucun Thème</h1>
