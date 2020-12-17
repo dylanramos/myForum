ca2886c6a4df055fc45ac9c898bbe96b5d32b027
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2020-12-17 10:34:48 +0100

    Have the theme display with all its topics

--- resources/views/themes/show.blade.php
+++ resources/views/themes/show.blade.php
@@ -1,5 +1,10 @@
 @extends ('layout')
 
 @section ('content')
-    <h1 class="text-center p-5">Show Theme <?= $id ?></h1>
+    <h1 class="text-center p-5">Theme: {{ $theme->name }}</h1>
+    @foreach ($theme->topics as $topic)
+        <div class="border border-dark p-2">
+            <a href="">{{ $topic->description }}</a>
+        </div>
+    @endforeach
 @endsection
