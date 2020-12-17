c18434cf354c216011ec5878f18b1d793e2c2c71
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2020-12-17 09:44:43 +0100

    Have the topics function which returns all the topics related to a theme

--- app/Models/Theme.php
+++ app/Models/Theme.php
@@ -10,4 +10,9 @@ class Theme extends Model
     use HasFactory;
 
     public $timestamps = false; // To disable the created_at and updated_at default columns
+
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
