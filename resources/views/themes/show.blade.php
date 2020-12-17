3a36da2d3b23f3d1b4d2caa60380719cbeefa1be
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2020-12-17 12:06:20 +0100

    Have the topics view

--- resources/views/themes/show.blade.php
+++ resources/views/themes/show.blade.php
@@ -4,7 +4,7 @@
     <h1 class="text-center p-5">Theme: {{ $theme->name }}</h1>
     @foreach ($theme->topics as $topic)
         <div class="border border-dark p-2">
-            <a href="">{{ $topic->description }}</a>
+            <a href="{{ route('topics.show',$topic->id) }}">{{ $topic->description }}</a>
             <span class="badge badge-light float-right">{{ $topic->opinions->count() }}</span>
         </div>
     @endforeach
