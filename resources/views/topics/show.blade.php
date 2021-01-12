22c7537d9ef6bca89b536501508efc453ee56f16
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-01-12 11:18:16 +0100

    Display the user comments and the points

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -1,20 +1,31 @@
 @extends ('layout')
 
-@section ('content')
-    <h3 class="p-2">Discussion autour du sujet "{{ $topic->description }}" dans le cadre du thème {{ $topic->theme->name }}:</h3>
-    <div class="small mb-3">Proposé par {{ $topic->forumuser->pseudo }}</div>
+@section('content')
+    <h3 class="p-2">Discussion autour du sujet "{{ $topic->description }}" dans le cadre du thème {{ $topic->theme->name }}:
+    </h3>
+    <div class="small mb-3">Proposé par {{ $topic->user->pseudo }}</div>
     @forelse ($topic->opinions as $opinion)
         <div class="p-2 ml-2">
-            ({{ $opinion->forumuser->first_name }})
+            ({{ $opinion->user->first_name }})
             {{ $opinion->description }}
-            @if($opinion->references->count() > 0)
+            @if ($opinion->references->count() > 0)
                 <div id="accordion{{ $opinion->id }}" class="small grey-text">
-                    <div data-toggle="collapse" data-target="#collapse{{ $opinion->id }}" aria-expanded="true" aria-controls="collapse{{ $opinion->id }}">
+                    <div data-toggle="collapse" data-target="#collapse{{ $opinion->id }}" aria-expanded="true"
+                        aria-controls="collapse{{ $opinion->id }}">
                         Références
                     </div>
                     <div id="collapse{{ $opinion->id }}" class="collapse" data-parent="#accordion{{ $opinion->id }}">
                         @foreach ($opinion->references as $reference)
                             <div class="bg-light p-1"><a href="{{ $reference->url }}">{{ $reference->description }}</a></div>
+                        @endforeach
+                    </div>
+                </div>
+            @endif
+            @if ($opinion->comments->count() > 0)
+                <div class="small ml-3">
+                    <div>
+                        @foreach ($opinion->comments as $comment)
+                            <div class="p-1">{{ $comment->pseudo }} : {{ $comment->pivot->comment }}, {{ $comment->pivot->points }}</div>
                         @endforeach
                     </div>
                 </div>
