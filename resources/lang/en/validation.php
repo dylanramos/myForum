e852439af2336dd8437918f84492eb61c6509aa1
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-01-19 10:47:51 +0100

    Change mimetypes

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -83,7 +83,7 @@ return [
         'array' => 'The :attribute may not have more than :max items.',
     ],
     'mimes' => 'The :attribute must be a file of type: :values.',
-    'mimetypes' => 'The :attribute must be a file of type: :values.',
+    'mimetypes' => 'The :attribute must be at least :min characters.',
     'min' => [
         'numeric' => 'The :attribute must be at least :min.',
         'file' => 'The :attribute must be at least :min kilobytes.',
