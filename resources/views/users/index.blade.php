abbb1322deccbc80157a89db4f530f2e4370bf80
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2021-01-28 11:29:03 +0100

    Display all users with their role

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,28 @@
+@extends ('layout')
+
+@section ('content')
+    <h1 class="text-center p-5">Utilisateurs</h1>
+    <div class="row">
+        <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Prénom</th>
+                <th scope="col">Nom</th>
+                <th scope="col">Pseudo</th>
+                <th scope="col">Rôle</th>
+              </tr>
+            </thead>
+        @forelse($users as $user)
+            <tr>
+                <td>{{ $user->first_name }}</td>
+                <td>{{ $user->last_name }}</td>
+                <td>{{ $user->pseudo }}</td>
+                <td>{{ $user->role->name }}</td>
+            </tr>
+        
+        @empty
+        <h1>Aucun utilisateur</h1>
+        @endforelse
+        </table>
+    </div>
+@endsection
